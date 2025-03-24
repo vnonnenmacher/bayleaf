757a417ed580833bd1ca89072bbecb2ec96ef1ac
Vinicius Nonnenmacher <vnonnenmacher@gmail.com>
2025-03-23 22:57:31 -0300

    add patient admin page

--- patients/admin.py
+++ patients/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from patients.models import Patient
+from users.admin import IdentifierInline
 
-# Register your models here.
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email', 'birth_date']
+    search_fields = ['first_name', 'last_name', 'email']
+    inlines = [IdentifierInline]
+    autocomplete_fields = ['address1', 'address2', 'primary_contact', 'secondary_contact']
+
+    fieldsets = (
+        ("Basic Info", {
+            'fields': ('first_name', 'last_name', 'email', 'birth_date', 'password')
+        }),
+        ("Address & Contact", {
+            'fields': ('address1', 'address2', 'primary_contact', 'secondary_contact'),
+            'classes': ('collapse',),
+        }),
+        # ❌ Don't include `groups`, `user_permissions`, `is_staff`, etc.
+    )
