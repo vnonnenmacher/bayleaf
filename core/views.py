09817a5837cebc7b6b564c21469fab2d1145331a
Vinicius Nonnenmacher <vnonnenmacher@gmail.com>
2025-09-20 01:38:29 -0300

    samples + health endpoint permissions

--- core/views.py
+++ core/views.py
@@ -2,10 +2,13 @@
 from rest_framework import viewsets, permissions
 from .models import Service
 from .serializers import ServiceSerializer
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+
 
 @api_view(["GET"])
+@permission_classes([AllowAny])
 def healthcheck(request):
     return Response({"status": "ok"}, status=200)
 
