8cf005d8c5239ba094595f18b23704cd6bded74b
Vinicius FN <vnonnenmacher@gmail.com>
2026-04-08 19:27:25 -0700

    deprecate coreapi on drf

--- bayleaf/settings/dev.py
+++ bayleaf/settings/dev.py
@@ -180,7 +180,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
     "DEFAULT_THROTTLE_CLASSES": ["rest_framework.throttling.ScopedRateThrottle"],
-    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.openapi.AutoSchema",
 }
 REST_FRAMEWORK.setdefault("DEFAULT_THROTTLE_RATES", {})
 REST_FRAMEWORK["DEFAULT_THROTTLE_RATES"].update(
